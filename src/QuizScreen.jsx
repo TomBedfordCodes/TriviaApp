b46828c811041f8af24fc8557d1b7bb17259c0ec
TomBCodes <thomas.bedford89@gmail.com>
2024-01-27 23:11:34 +0800

    Shuffle array of answers before render

--- src/QuizScreen.jsx
+++ src/QuizScreen.jsx
@@ -8,21 +8,25 @@ import { decode } from 'html-entities'
 export default function QuizScreen() {
 
     const [questionsData, setQuestionsData] = React.useState([])
+    const [isMarking, setIsMarking] = React.useState(false)
 
     React.useEffect(function() {
-        console.log("effect ran")
+        if (isMarking) {
+            return
+        }
         fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
             .then(res => res.json())
             .then(data => setQuestionsData(data.results))
             .catch(e => {
                 console.error(e);
             })
-    }, [])
+    }, [isMarking])
 
     const questionElements = questionsData.map(x => {
         const answersArr = x.incorrect_answers
         const randInt = Math.floor(Math.random() * 4)
         answersArr.splice(randInt, 0, x.correct_answer)
+        answersArr.sort(() => (Math.random() > .5) ? 1 : -1)
         return (
             <QuestionSection 
                 question={decode(x.question)}
