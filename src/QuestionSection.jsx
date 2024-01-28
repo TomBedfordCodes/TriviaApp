7951217d22610cf2dce2ff10dba1c520ce3878f0
TomBCodes <thomas.bedford89@gmail.com>
2024-01-28 21:38:07 +0800

    Refactor so state of all questions and answers is held within the QuizScreen component

--- src/QuestionSection.jsx
+++ src/QuestionSection.jsx
@@ -1,40 +1,19 @@
 import React from 'react'
 import AnswerBtn from './AnswerBtn'
-import { nanoid } from 'nanoid'
-import { decode } from 'html-entities'
 
 
 
 export default function QuestionSection(props) {
 
-    const [answers, setAnswers] = React.useState(() => ([
-        {answer: decode(props.answers[0]), id: nanoid(), isSelected: false},
-        {answer: decode(props.answers[1]), id: nanoid(), isSelected: false}, 
-        {answer: decode(props.answers[2]), id: nanoid(), isSelected: false}, 
-        {answer: decode(props.answers[3]), id: nanoid(), isSelected: false}
-    ]))
-
-    const answerElements = answers.map(x => {
+    const answerElements = props.answers.map(x => {
         return (
             <AnswerBtn 
                 key={x.id}
                 answer={x.answer}
                 isSelected={x.isSelected}
-                handleClick={() => answerSelected(x.id)}
+                handleClick={() => props.handleClick(x.questionNumber, x.id)}
             />)
     })
-
-    function answerSelected(id) {
-        const newArr = answers.map(answer => {
-           if (answer.id === id) {
-               return {...answer, isSelected: !answer.isSelected}
-           } else {
-               return {...answer, isSelected: false}
-           }
-       })
-       setAnswers(newArr)
-   }
-   
     return (
         <div className='question-section--container'>
             <h2>{props.question}</h2>
@@ -44,4 +23,3 @@ export default function QuestionSection(props) {
         </div>
     )
 }
-
--- src/QuizScreen.jsx
+++ src/QuizScreen.jsx
@@ -10,11 +10,30 @@ export default function QuizScreen() {
     const [questionsData, setQuestionsData] = React.useState([])
     const [isMarking, setIsMarking] = React.useState(false)
 
+    const [questionsAndAnswers, setQuestionsAndAnswers] = React.useState([])
+    // questionsAndAnswers array format:
+    // [
+    //     {
+    //         question: x.question,
+    //         answers: [
+                // {
+                    // questionNumber: i,
+                    // answer: decode(y),
+                    // id: nanoid(),
+                    // isSelected: false
+                // },
+    //         ],
+    //         correctAnswer: x.correct_answer
+    //     },
+    // ]
+
+    
+    // Get trivia data from API
     React.useEffect(function() {
         if (isMarking) {
             return
         }
-        fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
             .then(res => res.json())
             .then(data => setQuestionsData(data.results))
             .catch(e => {
@@ -22,25 +41,79 @@ export default function QuizScreen() {
             })
     }, [isMarking])
 
-    const questionElements = questionsData.map(x => {
-        const answersArr = x.incorrect_answers
-        const randInt = Math.floor(Math.random() * 4)
-        answersArr.splice(randInt, 0, x.correct_answer)
-        answersArr.sort(() => (Math.random() > .5) ? 1 : -1)
+
+    // Put data from API into an array of objects (questions and answers together)
+    React.useEffect(function() {
+        const newArr = questionsData.map((x, i) => {
+            const answersTempArr = x.incorrect_answers
+            const randInt = Math.floor(Math.random() * 4)
+            answersTempArr.splice(randInt, 0, x.correct_answer)
+            answersTempArr.sort(() => (Math.random() > .5) ? 1 : -1)
+            const answersArr = answersTempArr.map(y => {
+                return ({
+                        questionNumber: i,
+                        answer: decode(y),
+                        id: nanoid(),
+                        isSelected: false
+                    })
+            })
+            return ({
+                    question: x.question,
+                    answers: answersArr,
+                    correctAnswer: x.correct_answer
+                })
+        })
+        setQuestionsAndAnswers(newArr)
+    }, [questionsData])
+
+
+    // Set up question section elements, each has a question and four possible answers
+    const questionElements = questionsAndAnswers.map(x => {
         return (
-            <QuestionSection 
+            <QuestionSection
                 question={decode(x.question)}
-                answers={answersArr}
-                correctAnswer={x.correct_answer}
+                answers={x.answers}
+                correctAnswer={x.correctAnswer}
+                handleClick={answerSelected}
                 key={nanoid()}
             />
         )
     })
 
+    // When an answer button is clicked, find it in master array and change isSelected
+    function answerSelected(questionNumber, id) {
+        setQuestionsAndAnswers(oldArr => {
+            const newArr = oldArr.slice()
+            for (let answer of newArr[questionNumber].answers) {
+                if (answer.id === id) {
+                    answer.isSelected = !answer.isSelected
+                } else {
+                    answer.isSelected = false
+                }
+            }
+            return newArr
+        })
+    }
+
+
+
+
+
+    function checkAnswers() {
+        setIsMarking(true)
+        for (let qAndAObj of questionsAndAnswers) {
+            
+        }
+    }
+
+
+
+
+
     return (
         <div className="quiz--container">
             {questionElements}
-            <button className="checkanswers--btn">Check answers</button>
+            {!isMarking && <button className="checkanswers--btn" onClick={checkAnswers}>Check answers</button>}
         </div>
     )
 }
